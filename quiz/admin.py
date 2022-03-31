4cf00fe2c9f866d8e1519dfa3afd6f964b8677ce
morthell <morthell@gmail.com>
2022-03-31 21:23:52 +0200

    New models with images, and migrations

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Categories, Quiz
+from .models import Categories, Quiz, Images
 
 # Register your models here.
 admin.site.register(Categories)
 admin.site.register(Quiz)
+admin.site.register(Images)
--- quiz/models.py
+++ quiz/models.py
@@ -4,9 +4,11 @@ from django.db import models
 # Create your models here.
 
 class Images (models.Model):
-    filename = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
     image = models.ImageField(null=True)
 
+    def __str__(self):
+        return self.name
 
 
 class Categories(models.Model):
